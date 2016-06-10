09b7dd0cab12511b627cff0695c7c211fd54d736
Constantin Meinhardt <meinhardt@live.de>
2016-06-10 12:25:54 +0200

    Test mit C# und gitignore

---
+++ Einstieg/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Einstieg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "Servus Welt";
+            Console.WriteLine(test);
+            Console.Beep(200, 2000);
+            Console.ReadKey();
+        }
+    }
+}
